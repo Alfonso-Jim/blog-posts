be7e9617fb4be71dfc922a8d40f835a6305ca7db
Alfonso Jim <alfonsojim1897@gmail.com>
2021-05-18 13:45:22 +0200

    initial app setup

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  replaceMe: () =>
+    "This line tricks redux into thinking that it has a valid reducer, remember to change this line.",
+});
