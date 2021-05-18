c6a376c18d499a59a292f62069afc5bf17d080e2
Alfonso Jim <alfonsojim1897@gmail.com>
2021-05-18 17:36:52 +0200

    reducer skeleton

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
 
 export default combineReducers({
-  replaceMe: () =>
-    "This line tricks redux into thinking that it has a valid reducer, remember to change this line.",
+  posts: postsReducer,
 });
