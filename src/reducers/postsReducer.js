96804b68ee132dc6b03f36e76de703ab3a0d07d4
Alfonso Jim <alfonsojim1897@gmail.com>
2021-05-19 14:56:24 +0200

    added mapStateToProps & got response.data

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,10 +1,4 @@
 export default (state = [], action) => {
-  if (action.type === "FETCH_POST") {
-    return action.payload;
-  }
-
-  return state;
-
   switch (action.type) {
     case "FETCH_POSTS":
       return action.payload;
