18e38edfd06f0a887fd50330d7e7717723177534
Alfonso Jim <alfonsojim1897@gmail.com>
2021-05-20 15:14:04 +0200

    added action creator call inside action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,32 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+// Effective way to do it
+export const fetchPostsAndUsers = () => async (dispatch) => {
+  console.log("about to fetch posts!");
+  await dispatch(fetchPosts());
+  console.log("fetched posts!");
+};
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-// Fancy way to do it, memoizing once!
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
+
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// Fancy way to do it, memoizing once! but it only works once for each user... >_<
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
 
 // This also works!
 // export const fetchUser = _.memoize(function (id) {
